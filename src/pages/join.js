3c115f9242740a4f7a34f5a31d0ac99f1f46bbde
Norbert Dragán <norbert.dragan@gmail.com>
2019-06-16 14:50:42 +0800

    Add join page

---
+++ src/pages/join.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import Hero from "../components/Hero"
+import TherapList from "../components/TherapList"
+
+const JoinPage = () => (
+  <Layout>
+    <SEO title="Join" />
+    <Hero headline="Want to be listed on Theraplist? Fill out the form below to apply." />
+
+  </Layout>
+)
+
+export default JoinPage
